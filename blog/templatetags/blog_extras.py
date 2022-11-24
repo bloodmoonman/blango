7be446c40f2cde09b152b104062a5a65e8c3daad
Aykan Seper <aykanseper@gmail.com>
2022-11-24 20:08:38 +0000

    used template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django import template
 from django.utils.html import format_html
+from blog.models import Post
 
 #this is a custom filter 
 
@@ -10,12 +11,13 @@ user_model = get_user_model()
 
 
 @register.filter
-def author_details(author, current_user=None):
-    if not isinstance(author, user_model):  #to see if we are working with User object
+def author_details(author, current_user):
+    if not isinstance(author, user_model):
+        # return empty string as safe default
         return ""
 
     if author == current_user:
-      return format_html("<strong>me</strong>") #marking the return string as safe with format_html
+        return format_html("<strong>me</strong>")
 
     if author.first_name and author.last_name:
         name = f"{author.first_name} {author.last_name}"
@@ -30,4 +32,23 @@ def author_details(author, current_user=None):
         suffix = ""
 
     return format_html('{}{}{}', prefix, name, suffix)
+
+
+
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+
+
+@register.simple_tag
+def endrow():
+    return format_html("</div>")
+
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
+
 
