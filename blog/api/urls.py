14ff4b88acc927fea8b22ef506d557f93e93cbfd
Aykan Seper <aykanseper@gmail.com>
2023-01-12 18:56:04 +0000

    finish viewsets and routers

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -2,11 +2,16 @@
 from rest_framework.urlpatterns import format_suffix_patterns
 from django.urls import path, include
 from rest_framework.authtoken import views
-from blog.api.views import PostList, PostDetail, UserDetail
+from blog.api.views import UserDetail, TagViewSet, PostViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
 
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
+    #path("posts/", PostList.as_view(), name="api_post_list"), we use viewsets for list and detail
+    #path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
 ]
 
@@ -19,5 +24,7 @@ urlpatterns += [
 
 urlpatterns += [
     path("auth/", include("rest_framework.urls")),
-    path("token-auth/", views.obtain_auth_token)
-]
+    path("token-auth/", views.obtain_auth_token),
+    path("", include(router.urls)), #for instance you can set the url for 
+]                                          #path("blabla/", include(router.urls)) and it would
+                                           #work as /api/v1/blabla/tags tags here is from router.register("tags", TagViewSet)
